063ee4f6fc73257daf10114c34e1623610a4b268
ElenaIva <ei492493@gmail.com>
2024-09-12 14:21:53 +0200

    https://github.com/ElenaIva/1a.git

---
+++ 1a.py
@@ -0,0 +1,10 @@
+tasks_quantity = 12
+print(tasks_quantity)
+hours_quantity = 1.5
+print(hours_quantity)
+course_name = "Python"
+print(course_name)
+one_task_time = print(tasks_quantity)/print(one_task_time)
+print("Курс:", course_name, "всего задач:", tasks_quantity, "звтрачено часов:", hours_quantity,  "среднее время выполнения:", one_task_time, "часов")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
